3c0f1f5432c1a2efefe600fa30c3fdaf3db4bf12
shefali goel <goel136shefali@gmail.com>
2021-11-23 03:40:34 +0530

    fix: url

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import "./index.css";
 
 import { client } from "./client";
 import Categories from "./components/Category/categories";
-// import PopularMicrocourses from "./components/popularMicrocourses";
-// import FeaturedMicrocourse from "./components/featuredMicrocourses";
 import IndexCategory from "./components/CategoryFilter";
 
 class App extends React.Component {
   state = {
     categories: [],
     categoryData: [],
-    // popularCourse: [],
-    // featureCourse: [],
   };
 
   componentDidMount() {
@@ -22,29 +18,6 @@ class App extends React.Component {
       .then((response) => {
         for (const item of response.items) {
           console.log(item);
-          // if (item.sys.contentType.sys.id === "whatWouldYouLikeToLearnToday") {
-          //   this.setState({
-          //     categories: [...this.state.categories, item],
-          //   });
-          // } else if (item.sys.contentType.sys.id === "categoryAll") {
-          //   this.setState({
-          //     categoryData: [...this.state.categoryData, item],
-          //   });
-          // } else if (
-          //   item.sys.contentType.sys.id === "componentPopularMicrocourses"
-          // ) {
-          //   console.log("item", item);
-          //   this.setState({
-          //     popularCourse: [...this.state.popularCourse, item],
-          //   });
-          // } else if (
-          //   item.sys.contentType.sys.id === "componentFeaturedMicrocourses"
-          // ) {
-          //   console.log("item", item);
-          //   this.setState({
-          //     featureCourse: [...this.state.featureCourse, item],
-          //   });
-          // }
           if (item.sys.contentType.sys.id === "contentType") {
             this.setState({
               categoryData: [...this.state.categoryData, item],
@@ -68,12 +41,6 @@ class App extends React.Component {
             <div className="wrapper">
               <Categories categories={this.state.categoryData} />
               <IndexCategory categoryData={this.state.categoryData} />
-              {/* <PopularMicrocourses
-                popularMicrocourses={this.state.popularCourse}
-              />
-              <FeaturedMicrocourse
-                featuredMicroCourse={this.state.featureCourse}
-              /> */}
             </div>
           </main>
         </div>
